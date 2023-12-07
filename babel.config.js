9a81b9054b455a8f66c30011b1164b7cb786cb05
David Murdoch <187813+davidmurdoch@users.noreply.github.com>
2023-12-06 20:00:31 -0500

    update browserlist and babel targets to chrome/firefox 89 to add support for top level async

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,7 @@ module.exports = function (api) {
       strictMode: true,
     },
     targets: {
-      browsers: ['chrome >= 80', 'firefox >= 78'],
+      browsers: ['chrome >= 89', 'firefox >= 89'],
     },
     presets: [
       '@babel/preset-typescript',
